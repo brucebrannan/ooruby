1325251129d92a3380d997f96f2170a01a25d4da
Bruce Brannan <bruce.brannan@gmail.com>
2012-04-10 23:05:48 -0600

    first examples

---
+++ rubyclassdefinition.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+#When you define a class, you define a blueprint for a data type.
+#It defines what the class means, what an object of the class will
+#consist of and what operations can be performed on such an object.
+
+#A class definition starts with the keyword class followed by the class name
+#and is delimited with an end
+
+#Example:
+#class Box
+  #code
+#end
+
+#Use CamelCase for class names
+
