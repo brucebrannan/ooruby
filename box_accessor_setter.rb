b140e46fad8c5239536bbaa4e105b6643b08dfc4
Bruce Brannan <bruce.brannan@gmail.com>
2012-04-13 19:16:03 -0600

    adding class example

---
+++ box_accessor_setter.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+#The initialize method:
+#Useful when you want to initialize some class variables at the
+#time of object creation
+#Preceded by def
+
+#The instance variables:
+#A kind of class attributes and they become properties of objects
+#once the objects are created using the class
+#Every object's attributes are assigned individually and share no value
+#with other objects.
+#They are accessed using the @ operator within the class but to access
+#them outside the class we use public methods which are called accessor
+#methods.
+
+#The accessor and setter methods:
+#to make the variables available fro outside the class they must be defined
+#within accessor methods, these accessor methods are also known as getter
+#methods.
+
+#Example of class definition, initialize method, accessor & setter methods
